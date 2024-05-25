c4ad288a84ba2ba3d4c23ed8a17f925e84260732
SOMNATH PAULCHOUDHURY <114329091+00aimlds00@users.noreply.github.com>
2024-05-25 12:22:14 +0530

    Update quickupload_csv_to_mysql.py

--- quickupload_csv_to_mysql.py
+++ quickupload_csv_to_mysql.py
@@ -1,1 +1,18 @@
+"""
+SUPPOSE WE WILL UPLOAD ALL DATA FROM markspb2.csv TO scorespb TABLE
+"""
 
+import os
+os.chdir("PATHTOCSVFILE")
+import mysql.connector
+spcdatabase=mysql.connector.connect(
+    user="root",
+    host="localhost",
+    password="YOURPASSWORD",
+    database="DATABASENAME",
+    allow_local_infile=True
+    )
+sql_command = """LOAD DATA LOCAL INFILE 'markspb2.csv' INTO TABLE scorespb FIELDS TERMINATED BY ',' IGNORE 1 LINES; """
+spccur=spcdatabase.cursor()
+spccur.execute(sql_command)
+spcdatabase.commit()
